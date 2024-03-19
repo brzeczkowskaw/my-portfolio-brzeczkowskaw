b5fcb3d9c84162fac695173db8d8923a8dddea5c
Weronika Brzeczkowska-Kuzianik <brzeczkowska.w@gmail.com>
2024-03-19 22:48:27 +0100

    CV page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,16 +2,19 @@
 import { ref } from 'vue'
 import Tools from '../components/Tools.vue'
 import Contact from '../components/Contact.vue'
+import CvPage from '../components/CvPage.vue'
 
 </script>
 
 <template>
   <v-container fluid>
     <div class="part-height">info</div>
+    <div id="cv" class="part-height">
+      <CvPage />
+    </div>
     <div id="tools" class="part-height">
       <Tools />
     </div>
-    <div id="cv" class="part-height">projects</div>
     <div id="page-projects" class="part-height">projects</div>
     <div id="ui-projects" class="part-height">ui projects</div>
     <div id="contact">
